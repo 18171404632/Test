27ab9a0865716318b83fb943fb54bc1126ff69ec
g_Newkey <g_847749909@qq.com>
2020-08-09 17:47:42 +0800

    //初始化

---
+++ test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("111111111111111111")
+	fmt.Println("222222222222222222")
+	fmt.Println("333333333333333333")
+	fmt.Println("444444444444444444")
+	fmt.Println("555555555555555555")
+	fmt.Println("666666666666666666")
+	fmt.Println("777777777777777777")
+}
